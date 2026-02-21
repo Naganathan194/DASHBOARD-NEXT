739ad2701f09d04ffe65a4679c3d4cea5bd90380
Naganathan S <145862504+Naganathan194@users.noreply.github.com>
2026-02-21 20:04:44 +0530

    Delete .next directory

--- .next/dev/static/chunks/app_page_tsx_893e5ca7._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_fd9cb2e3._.js",
-  "static/chunks/_e98d152b._.js",
-  "static/chunks/node_modules_next_dist_compiled_react_583fbf25._.js"
-],
-    source: "dynamic"
-});
